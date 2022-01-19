020b05178366f6885a9721e338b79e0c55bd7148
Eduardo Piorini <edupiorini@gmail.com>
2022-01-19 06:25:04 -0300

    user table bugs fixed

---
+++ user-api/src/main/resources/db/migration/V3__create_user_table.sql
@@ -0,0 +1,9 @@
+create table users.user (
+    id bigserial primary key,
+    name varchar(100) not null,
+    cpf varchar(100) not null,
+    address varchar(100) not null,
+    email varchar(100) not null,
+    phone_number varchar(100) not null,
+    creation_date timestamp not null
+);
